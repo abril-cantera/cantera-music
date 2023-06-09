6a8ba4b77e5e10d3c4e8e62a30d466585cbdd15b
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-06-09 14:00:48 -0300

    Reiniciando db

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
