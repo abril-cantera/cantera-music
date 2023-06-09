a094048bc5d69da87eebecb6d36e4764fdc4eb9a
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-06-09 12:20:37 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fileUpload = require('express-fileupload');
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(fileUpload({
   useTempFiles: true,
