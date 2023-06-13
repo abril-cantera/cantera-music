31de5ef7254604a850bb0a7d4ba187073e1ccc6a
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-06-13 14:22:35 -0300

    Quitando opciones de CORS

--- index.js
+++ index.js
@@ -13,17 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  }
-}
-app.use(cors(options));
+app.use(cors());
 
 app.use(fileUpload({
   useTempFiles: true,
